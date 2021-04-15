f74bf45f133fc39a4844b9ca5235cbc2bc79b826
sina <ksina.72@gmail.com>
2021-04-15 17:54:34 +1200

    working on db

---
+++ server/db/db.js
@@ -0,0 +1,2 @@
+const config = require('./knexfile').development
+const db = require('knex')(config)
---
+++ server/db/migrations/migrations/20210415053453_quotes.js
@@ -0,0 +1,11 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('Quotes', (table) => {
+    table.increments('id').primary()
+    table.string('quote')
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('Quotes')
+}
