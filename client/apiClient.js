7e4228def08c06f2e5811b24239299a3a3fecbf4
Jared Pinfold <77950295+jared-pinfold@users.noreply.github.com>
2021-04-16 11:50:44 +1200

    created getQuote function in apiClient.js

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,6 +1,6 @@
 import request from 'superagent'
 
-const quoteUrl = '/api/v1/widgets/' // will need to update once db route is done
+const quoteUrl = '/api/v1/quote' // will need to update once db route is done
 
 export function getQuote () {
   return request
