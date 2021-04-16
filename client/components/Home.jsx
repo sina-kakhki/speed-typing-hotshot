3e905ff7bb10242118149dec013289617fe03101
sina <ksina.72@gmail.com>
2021-04-16 15:07:23 +1200

    added logo

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,14 +8,14 @@ function Home () {
     }
     return (
         <>
-        <div className="flex items-center">
+        <container className="flex">
           <Link to="/" >
-            <button className="m-7 p-7 border-4  text-6xl">Home</button>
+            <button className="m-7 p-7 border-4 text-6xl hover:bg-red-700">Home</button>
           </Link>
-        </div>
-        <div>
+          <div className="ml-72">
           <img src='./images/Speed-movie-logo.png' alt='logo'></img>
-        </div>
+          </div>
+        </container>
         <div className="absolute bottom-0 left-0 p-11 align-text-bottom text-6xl ">
             <h2>Team Speed 2021 ©</h2>
         </div>
